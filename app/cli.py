6bcc44c1ef4d103d3a764710dc2b51af986facf9
UbaydR <131396971+UbaydR@users.noreply.github.com>
2026-02-27 20:23:21 -0400

    Completed main lab and exercises

--- app/cli.py
+++ app/cli.py
@@ -1,3 +1,4 @@
+from typing import Annotated
 import typer
 from app.database import create_db_and_tables, get_session, drop_all
 from app.models import User
@@ -7,7 +8,7 @@ from sqlalchemy.exc import IntegrityError
 
 cli = typer.Typer()
 
-@cli.command()
+@cli.command(help= "Initialize the database with a default user (bob)")
 def initialize():
     with get_session() as db: # Get a connection to the database
         drop_all() # delete all tables
@@ -18,32 +19,90 @@ def initialize():
         db.refresh(bob) # Update the user (we use this to get the ID from the db)
         print("Database Initialized")
 
-@cli.command()
+@cli.command(help= "Get a user by username and print it to the console")
 def get_user(username:str):
-    # The code for task 5.1 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    with get_session() as db:
+        user = db.exec(select(User).where(User.username == username)).first()
+        if not user:
+            print(f'{username} not found!')
+            return
+        print(user)
 
-@cli.command()
+@cli.command(help = "Get all users and print them")
 def get_all_users():
-    # The code for task 5.2 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    with get_session() as db:
+        all_users = db.exec(select(User)).all()
+        if not all_users:
+            print("No users found")
+        else:
+            for user in all_users:
+                print(user)
 
-
-@cli.command()
+@cli.command(help = "Change a user's email address by username")
 def change_email(username: str, new_email:str):
-    # The code for task 6 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    with get_session() as db: # Get a connection to the database
+        user = db.exec(select(User).where(User.username == username)).first()
+        if not user:
+            print(f'{username} not found! Unable to update email.')
+            return
+        user.email = new_email
+        db.add(user)
+        db.commit()
+        print(f"Updated {user.username}'s email to {user.email}")
 
-@cli.command()
+@cli.command(help= "Create a new user with the given username, email and password")
 def create_user(username: str, email:str, password: str):
-    # The code for task 7 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    with get_session() as db: # Get a connection to the database
+        newuser = User(username, email, password)
+        try:
+            db.add(newuser)
+            db.commit()
+        except IntegrityError as e:
+            db.rollback()
+            print("Username or email already taken!")
+        else:
+            print(newuser)
 
-@cli.command()
+@cli.command(help = "Delete a user by the given username")
 def delete_user(username: str):
-    # The code for task 8 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    with get_session() as db:
+        user = db.exec(select(User).where(User.username == username)).first()
+        if not user:
+            print(f'{username} not found! Unable to delete user.')
+            return
+        db.delete(user)
+        db.commit()
+        print(f'{username} deleted')
 
+
+#exercises
+
+#excerise 1
+@cli.command(help= "Find users by partial match of username or email")
+def find_user(
+    query:Annotated[str,typer.Argument(help = "Partial match for username or email")]
+    ):
+    with get_session() as db:
+        users = db.exec(select(User).where((User.username.contains(query)) | (User.email.contains(query)) )).all()
+        if not users:                  
+            print(f'No users found matching "{query}"')       
+            return                      
+        for user in users:              
+            print(user)
+
+
+#excerise 2
+@cli.command(help= "List users using pagination with limit and offset")
+def paginated_table(
+    limit:int=typer.Option(10,help="10 users returned per page."),
+    offset:int=typer.Option(0,help="Number of users to skip before starting to return results."),
+):
+    with get_session() as db:
+        users=db.exec(select(User).offset(offset).limit(limit)).all()
+        if not users:
+            print("No users found")
+        for user in users:
+            print(user)
 
 if __name__ == "__main__":
     cli()
